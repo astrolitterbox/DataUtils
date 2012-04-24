043a7c9a4695edba233a859e6fa28276689b52e0
Astrolitterbox <opit@hardcore.lt>
2012-04-24 13:50:47 +0200

    temporary test code with self-adjusting accuracy parameter, slow

--- readAtlas.py
+++ readAtlas.py
@@ -23,7 +23,7 @@ def find_mag(ra, dec):
       #else:
 	#print 'object not found!'
 
-def find_in_atlas(arg, ra, dec):
+def find_in_atlas(arg, ra, dec, roundParam):
 
     atlasFile = '../data/NSAtlas/nsa_v0_1_2.fits'
     atlas = pyfits.open(atlasFile)
@@ -40,23 +40,41 @@ def find_in_atlas(arg, ra, dec):
     ret = np.empty((len(ra), 3))
     for i in range(0, len(ra)):
       
-      decs = np.where(np.round(atlasData.field('DEC'), 3) == round(dec[i], 3))
-      ras = np.where(np.round(atlasData.field('RA'), 3) == round(ra[i], 3))
+      decs = np.where(np.round(atlasData.field('DEC'), roundParam) == round(dec[i], roundParam))
+      ras = np.where(np.round(atlasData.field('RA'), roundParam) == round(ra[i], roundParam))
       #print round(ras, 3), round(decs, 3)
-      print 'ra: ', ras[0], 'dec', decs[0]
+      print 'ra: ', ras[0], 'dec', decs[0], 'i', i
     
       a_multiset = collections.Counter(list(ras[0]))
       b_multiset = collections.Counter(list(decs[0]))
       overlap = list((a_multiset & b_multiset).elements())
-      print 'o', overlap
-      if overlap != []:
+      print 'o', overlap, len(overlap), roundParam
+      if len(overlap) == 1:
 	ret[i][0] =  atlasData.field('ra')[overlap]
 	ret[i][1] = atlasData.field('dec')[overlap]      
-	ret[i][2] =  atlasData.field(arg)[overlap]
+	ret[i][2] =  round(atlasData.field(arg)[overlap], 1)
+      elif overlap == []:
+	try:
+	  ret[i][0] =  ra[i]
+	  ret[i][1] = dec[i]
+	  print 'repeat', find_in_atlas(arg, ra[i], dec[i], roundParam-1)[:, 2]
+	  ret[i][2] = find_in_atlas(arg, ra[i], dec[i], roundParam-1)[:, 2]
+	except TypeError:
+	  print 'not detected'
+	  ret[i][0] =  ra[i]
+	  ret[i][1] = dec[i]	
+	  ret[i][2] = 0	  
       else:
-	ret[i][0] =  ra[i]
-	ret[i][1] = dec[i]
-	ret[i][2] = 0
+	try:
+	  ret[i][0] =  ra[i]
+	  ret[i][1] = dec[i]
+	  print 'repeat', find_in_atlas(arg, ra[i], dec[i], roundParam+1)[:, 2]
+	  ret[i][2] = find_in_atlas(arg, ra[i], dec[i], roundParam+1)[:, 2]
+	except TypeError:
+	  ret[i][0] =  ra[i]
+	  ret[i][1] = dec[i]
+	  print 'too many values, ambiguous detection'
+	  ret[i][2] = 0
       print 'sersic index ', ret[i]
     
     atlas.close()	
