23b35750b1ef78161f61df06d62919037482c7f7
Astrolitterbox <opit@hardcore.lt>
2012-04-24 12:14:47 +0200

    passing an array of values instead of looping through them

--- readAtlas.py
+++ readAtlas.py
@@ -28,8 +28,7 @@ def find_in_atlas(arg, ra, dec):
     atlasFile = '../data/NSAtlas/nsa_v0_1_2.fits'
     atlas = pyfits.open(atlasFile)
     atlasData = atlas[1].data
-#    print atlas[1].columns.names
-    
+#    print atlas[1].columns.names  
     head = atlas[1].header
     #print atlasData.field('RA')[15], atlasData.field('DEC')[15]
     '''for i in range(0, atlasData.shape[0]):
@@ -38,23 +37,22 @@ def find_in_atlas(arg, ra, dec):
 	return atlasData.field(arg)[i]
       #else:
 	#print '''
-    decs = np.where(np.round(atlasData.field('DEC'), 3) == round(dec, 3))
-    ras = np.where(np.round(atlasData.field('RA'), 3) == round(ra, 3))
-    print round(ra, 3), round(dec, 3)
-    print ras[0]
-    print decs[0]
+    ret = np.empty((len(ra), 3))
+    for i in range(0, len(ra)):
+      decs = np.where(np.round(atlasData.field('DEC'), 3) == round(dec[i], 3))
+      ras = np.where(np.round(atlasData.field('RA'), 3) == round(ra[i], 3))
+      #print round(ras, 3), round(decs, 3)
+      print 'ra: ', ras[0], 'dec', decs[0]
     
+      a_multiset = collections.Counter(list(ras[0]))
+      b_multiset = collections.Counter(list(decs[0]))
+      overlap = list((a_multiset & b_multiset).elements())
+      if overlap != []:
+	ret[i] =  atlasData.field(arg)[overlap]
+      else:
+	ret[i] = 0
+      print 'sersic index ', ret[i]
     
-    #if list(ras) == list(decs):
-    
-    a_multiset = collections.Counter(list(ras[0]))
-    b_multiset = collections.Counter(list(decs[0]))
-    overlap = list((a_multiset & b_multiset).elements())
-    if overlap != []:
-      ret =  atlasData.field(arg)[overlap]
-    else:
-      ret = 0
-    print ret, 'returning '
     atlas.close()	
     return ret
     #print np.where((np.round(atlasData.field('RA'), 3) == round(ra, 3)))# and np.round(atlasData.field('DEC'), 3) == round(dec, 3)))
